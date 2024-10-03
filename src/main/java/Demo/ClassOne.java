4716969d99aacfd1be9f4b5634b6911ba149be69
Sakthi Madhu <151857561+sakthimadhu07@users.noreply.github.com>
2024-10-03 13:47:57 +0530

    Update ClassOne.java

--- src/main/java/Demo/ClassOne.java
+++ src/main/java/Demo/ClassOne.java
@@ -6,5 +6,6 @@ public class ClassOne {
 		System.out.println("Class one");
 		System.out.println("Class one plus");
 		System.out.println("Class one plus plus - REMOTE");
+		System.out.println("Class one plus plus - REMOTE");
 	}
 }
